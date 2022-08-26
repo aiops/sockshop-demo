59649d5b1516d0a81e5fe6ceba4345602eab9ef8
snedelkoski <sashonedelkoski@gmail.com>
2022-08-26 09:45:16 +0200

    main working

--- deployment/docker-compose.sh
+++ deployment/docker-compose.sh
@@ -9,5 +9,5 @@ port=$6
 
 sh fluent_bit_config_generator.sh "tail" "/host/var/lib/docker/containers/*/*.log" "*" "$repository" "$version" "log" "$url" "$port" "$username" "$password" > ./fluent-bit.conf
 docker-compose up -d
-sleep 150s
+sleep 90s
 docker-compose down
